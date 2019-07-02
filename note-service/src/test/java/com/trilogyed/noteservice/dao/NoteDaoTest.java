3a8f3d674793f261b4dc635743102b57bae8fdcf
tanz <contact@johntansor.dev>
2019-07-02 15:28:04 -0500

    adds noteDao, noteDaoTest; updates gitIgnore

---
+++ note-service/src/test/java/com/trilogyed/noteservice/dao/NoteDaoTest.java
@@ -0,0 +1,37 @@
+package com.trilogyed.noteservice.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoteDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addNote() {
+    }
+
+    @Test
+    public void getNote() {
+    }
+
+    @Test
+    public void getNoteByBook() {
+    }
+
+    @Test
+    public void getAllNotes() {
+    }
+
+    @Test
+    public void updateNote() {
+    }
+
+    @Test
+    public void deleteNote() {
+    }
+}
