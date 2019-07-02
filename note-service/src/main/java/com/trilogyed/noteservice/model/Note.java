cfe2e5594b8df1711a8b3b93292c92f2a946e856
tanz <contact@johntansor.dev>
2019-07-02 15:16:28 -0500

    adds note model

--- note-service/src/main/java/com/trilogyed/noteservice/model/Note.java
+++ note-service/src/main/java/com/trilogyed/noteservice/model/Note.java
@@ -1,4 +1,50 @@
 package com.trilogyed.noteservice.model;
 
 public class Note {
+
+    private int noteId;
+    private int bookId;
+    private String note;
+
+    public Note() {
+    }
+
+    public Note(int noteId, int bookId, String note) {
+        this.noteId = noteId;
+        this.bookId = bookId;
+        this.note = note;
+    }
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "noteId=" + noteId +
+                ", bookId=" + bookId +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
