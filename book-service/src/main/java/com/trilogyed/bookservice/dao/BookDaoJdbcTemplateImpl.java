a78b929b974b96e5e154a2ec53476f331dd4eddc
FoXoLoT <KevinTheLago@hotmail.com>
2019-07-02 15:26:50 -0500

    DAO, JDBC implementation, Unit Tests, and Book Model

--- book-service/src/main/java/com/trilogyed/bookservice/dao/BookDaoJdbcTemplateImpl.java
+++ book-service/src/main/java/com/trilogyed/bookservice/dao/BookDaoJdbcTemplateImpl.java
@@ -1,4 +1,123 @@
 package com.trilogyed.bookservice.dao;
 
-public class BookDaoJdbcTemplateImpl {
+import com.trilogyed.bookservice.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class BookDaoJdbcTemplateImpl implements BookDao {
+
+    private static final String INSERT_BOOK =
+            "insert into book (title, author) values (?, ?)";
+    private static final String SELECT_BOOK =
+            "select * from book where book_id = ?";
+    private static final String SELECT_ALL_BOOKS =
+            "select * from book";
+    private static final String UPDATE_BOOK =
+            "update book set title = ?, author = ? where book_id = ?";
+    private static final String DELETE_BOOK =
+            "delete from book where book_id = ?";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public BookDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public Book addBook(Book book) {
+
+        jdbcTemplate.update(
+                INSERT_BOOK,
+                book.getTitle(),
+                book.getAuthor()
+        );
+
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+        book.setBook_id(id);
+
+        return book;
+
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+
+        List<Book> books =
+                jdbcTemplate.query(
+                        SELECT_ALL_BOOKS,
+                        this::mapRowToBook
+                );
+
+        return books;
+
+    }
+
+    @Override
+    public Book getBook(int book_id) {
+        try {
+            return jdbcTemplate.queryForObject(SELECT_BOOK, this::mapRowToBook, book_id);
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    public Book updateBook(Book book) {
+
+        jdbcTemplate.update(UPDATE_BOOK,
+                book.getTitle(),
+                book.getAuthor(),
+                book.getBook_id()
+        );
+
+        try {
+
+            book = jdbcTemplate.queryForObject(SELECT_BOOK, this::mapRowToBook, book.getBook_id());
+            jdbcTemplate.update(DELETE_BOOK, book.getBook_id());
+
+            return book;
+
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
+
+    }
+
+    @Override
+    public Book deleteBook(int book_id) {
+
+        try {
+
+            Book book = jdbcTemplate.queryForObject(SELECT_BOOK, this::mapRowToBook, book_id);
+            jdbcTemplate.update(DELETE_BOOK, book_id);
+
+            return book;
+
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
+
+    }
+
+    private Book mapRowToBook(ResultSet rs, int rowNum) throws SQLException {
+
+        Book book = new Book();
+        book.setBook_id(rs.getInt("book_id"));
+        book.setTitle(rs.getString("title"));
+        book.setAuthor(rs.getString("author"));
+
+        return book;
+
+    }
+
 }
--- book-service/src/test/java/com/trilogyed/bookservice/dao/BookDaoTests.java
+++ book-service/src/test/java/com/trilogyed/bookservice/dao/BookDaoTests.java
@@ -1,4 +1,81 @@
 package com.trilogyed.bookservice.dao;
 
+import com.trilogyed.bookservice.model.Book;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BookDaoTests {
+
+    @Autowired
+    BookDao dao;
+
+    @Before
+    public void setUp() throws Exception {
+
+        List<Book> books = dao.getAllBooks();
+
+        books.stream().forEach(book -> dao.deleteBook(book.getBook_id()));
+
+    }
+
+    @Test
+    public void addGetDeleteBook() {
+
+        Book book = new Book(2, "Title", "Author");
+
+        book = dao.addBook(book);
+
+        Book fromDao = dao.getBook(book.getBook_id());
+
+        assertEquals(fromDao, book);
+
+        dao.deleteBook(book.getBook_id());
+
+        fromDao = dao.getBook(book.getBook_id());
+
+        assertNull(fromDao);
+
+    }
+
+    @Test
+    public void getAllBooks() {
+
+        Book book = new Book(2, "Title", "Author");
+        dao.addBook(book);
+
+        book = new Book(5, "Another Title", "Another Author");
+        dao.addBook(book);
+
+        List<Book> books = dao.getAllBooks();
+
+        assertEquals(2, books.size());
+
+    }
+
+    @Test
+    public void updateBook() {
+
+        Book book = new Book(2, "Title", "Author");
+        book = dao.addBook(book);
+
+        book.setTitle("Different Title");
+        book.setAuthor("Different Author");
+
+        dao.updateBook(book);
+        Book fromDao = dao.getBook(book.getBook_id());
+
+        assertEquals(book, fromDao);
+
+    }
+
 }
