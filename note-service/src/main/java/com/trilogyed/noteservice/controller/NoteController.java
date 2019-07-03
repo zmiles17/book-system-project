c2b6641bfa4e38079cfcd7bd7b6289ce4a50966f
tanz <contact@johntansor.dev>
2019-07-03 10:18:59 -0500

    fixes getNoteByBook returntype, adds tesing

--- note-service/src/main/java/com/trilogyed/noteservice/controller/NoteController.java
+++ note-service/src/main/java/com/trilogyed/noteservice/controller/NoteController.java
@@ -1,4 +1,13 @@
 package com.trilogyed.noteservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/note")
 public class NoteController {
+
+//    @Autowired
+//
 }
