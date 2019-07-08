370af475b765877fe5a6e73203ffaa10fdd8c912
nika <kulz.nurgul@gmail.com>
2019-07-08 14:31:35 -0500

    swagger

--- note-service/src/main/java/com/trilogyed/noteservice/controller/NoteController.java
+++ note-service/src/main/java/com/trilogyed/noteservice/controller/NoteController.java
@@ -1,6 +1,7 @@
 package com.trilogyed.noteservice.controller;
 
 import com.trilogyed.noteservice.dao.NoteDao;
+import com.trilogyed.noteservice.exception.NotFoundException;
 import com.trilogyed.noteservice.model.Note;
 import com.trilogyed.noteservice.service.NoteServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,11 @@ public class NoteController {
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Note getNote(@PathVariable("id") int id) {
-        return noteDao.getNote(id);
+        Note note= noteDao.getNote(id);
+        if(note == null){
+            throw new NotFoundException("Task could not be retrieved for id " + id);
+        }
+        return note;
     }
 
     @GetMapping("/book/{book_id}")
@@ -49,6 +54,9 @@ public class NoteController {
     public void updateNote(@PathVariable("id") int noteId,
                            @RequestBody @Valid Note note) {
         service.updateNote(noteId, note);
+        if (noteId != note.getNoteId()) {
+            throw new IllegalArgumentException("Note ID on path must match the ID in the Note object");
+        }
     }
 
     @DeleteMapping("/{id}")
